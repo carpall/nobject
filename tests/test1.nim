4b64466d67bc9172b050a77bfc26261e587e28a4
Carpall <juniormondi@gmailcom>
2021-01-25 10:07:11 +0100

    updating tests

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,24 +1,22 @@
 import unittest
 
 import nobject
-test "Comparing Objects Test ^":
+test "Comparing Objects Test":
   let obj: Object = 2
   let obj1: Object = null
-  echo "obj == obj1: ", obj == obj1
-test "Instance Test ^":
+  echo obj == obj1
+test "Instance Test":
   type
     Person = object
       name: string
       age: uint8
   var me = Person(name: "Ciao")
   let obj: Object = newObject[Person](me)
-  echo "obj: ", (obj as Person)
-test "Bool Test ^":
+  assert (obj as Person).name == "Ciao"
+test "Bool Test":
   var u: Object = true
-  if u:
-    echo "!"
-  echo u
-test "Add Test ^":
-  test "add string ^":
+  assert u
+test "Add Test":
+  test "add string":
     var s: Object = "1"
-    echo s+"2"
+    assert s+"2" == "12"
